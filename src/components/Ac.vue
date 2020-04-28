b43bf22cd5043c0f33516d49ceac328ad7fac7b7
Isak <isakulstrup@gmail.com>
2020-04-28 16:17:30 +0200

    minor visual changes

--- src/components/Ac.vue
+++ src/components/Ac.vue
@@ -4,8 +4,9 @@
     v-bind:class="{ highlight: active }"
   >
     <input
-      class="acIn i input p3 fa"
+      class="acIn input p3 fa"
       type="text"
+      autocomplete=off
       name="ac.in"
       v-model="acData.in"
       @keyup="save"
@@ -14,8 +15,9 @@
     >
 
     <input
-      class="acOut i input p3 fa"
+      class="acOut input p3 fa"
       type="text"
+      autocomplete=off
       name="ac.out"
       v-model="acData.out"
       @keyup="save"
@@ -44,7 +46,6 @@ export default {
   },
   methods: {
     activate() {
-      // console.log(this.$refs.li)
       this.active = true
     },
     deactivate() {
@@ -71,20 +72,19 @@ export default {
 .ac {
   transition: all 0.4s ease-out;
   border: 1px solid #333;
-  overflow: hidden;
+  /* overflow: hidden; */
 }
 .ac:hover {
   background: #222;
   box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
 }
-.ac input {
-  border: 1px solid #151515;
-}
 .input {
   outline: none;
+  border: none;
+  overflow: hidden;
+  border-right: 1px solid #333;
 }
 .input:focus {
-  /* border: 1px solid rgb(247, 5, 255) */
   box-shadow: 0 0 6px rgba(15, 154, 189, 0.8) inset;
 }
 .acIn {
@@ -95,15 +95,14 @@ export default {
 }
 .highlight {
   background: #222;
-  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.6);
-  transform: scale(1.03);
+  box-shadow: 1px 1px 8px rgba(0, 0, 0, 0.6);
+  transform: scale(1.04);
   border: 3px solid rgb(15, 154, 189);
 }
 a {
   color: #555;
   flex: 1 0 fit-content;
   text-decoration: none;
-  display: block;
   border-radius: 0 3px 3px 0;
   cursor: pointer;
 }
