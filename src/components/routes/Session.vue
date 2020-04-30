81906870d71db8cb6e15cfe86bef6d3c8359a918
Isak <isakulstrup@gmail.com>
2020-04-30 15:39:51 +0200

    implemented new session endpoint

--- src/components/routes/Session.vue
+++ src/components/routes/Session.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="Session">
-    <div>
+    <div class="mv2">
       <h3>New session</h3>
-      <input type="text" name="host" placeholder="url" v-model="connectionDetails.host">
-      <input type="text" name="port" placeholder="port" v-model="connectionDetails.port">
-      <input type="text" name="parameters" placeholder="parameters" v-model="connectionDetails.parameters">
-      <input type="button" value="New" @click="newSession">
+      <input class="p3 br2 w100" type="text" name="host" placeholder="url" v-model="connectionDetails.host">
+      <input class="p3 br2 w100" type="text" name="port" placeholder="port" v-model="connectionDetails.port">
+      <input class="p3 br2 w100" type="button" value="New" @click="newSession">
     </div>
 
-    <div>
+    <div class="mv2">
       <h3>Connect to session</h3>
-      <input type="text" name="host" placeholder="url" v-model="connectionDetails.host">
-      <input type="text" name="port" placeholder="port" v-model="connectionDetails.port">
-      <input type="button" value="Connect" @click="connect">
+      <input class="p3 br2 w100" type="text" name="host" placeholder="url" v-model="connectionDetails.host">
+      <input class="p3 br2 w100" type="text" name="port" placeholder="port" v-model="connectionDetails.port">
+      <input class="p3 br2 w100" type="text" name="session" placeholder="session" v-model="connectionDetails.sessionId">
+      <input class="p3 br2 w100" type="button" value="New" @click="connect">
     </div>
 
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Session',
   data: () => {
@@ -27,23 +29,37 @@ export default {
         protocol: 'ws'
       },
       socketData: null,
-      socket: null
+      status: 'idle',
+      socket: null,
+      wsLog: ''
     }
   },
   methods: {
     newSession() {
-      // console.log('new session: ', this.connectionDetails.host, ':' , this.connectionDetails.port)
-      this.socket = new WebSocket(`${this.connectionDetails.protocol}://${this.connectionDetails.host}:${this.connectionDetails.port}/${this.connectionDetails.parameters}`)
-      this.socket.onopen = function() {
-        console.log("[open] Connection established")
-      }
-      this.socket.onmessage = function(event) {
-        console.log(`[message] Data received from server: ${event.data}`);
-        // logElement.innerHTML += ' ' + event.data
-      };
+      axios.get(`http://${this.connectionDetails.host}:${this.connectionDetails.port}/new`)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      }); 
     },
     connect() {
-      console.log('Connect to session: ', this.connectionDetails.host, ':', this.connectionDetails.port)
+      this.socket = new WebSocket(`${this.connectionDetails.protocol}://${this.connectionDetails.host}:${this.connectionDetails.port}/?id=${this.connectionDetails.sessionId}`)
+      this.socket.onopen = function() {
+        console.log("[open] Connection established")
+        // this.wsLog += '\n[open] Connection established'
+      }
+      this.socket.onmessage = function(event) {
+        console.log(`[message] Data received from server: ${event.data}`)
+        // this.wsLog += `\n[message] Data received from server: ${event.data}`
+      }
+      this.socket.onclose = function(event) {
+        console.log('Connection closed: ', event.code, event.reason)
+      }
     }
   }
 }
