4d5225e809cd2fc9a3205457b0b0bc2a3f4a7427
Isak <isakulstrup@gmail.com>
2020-05-01 17:34:36 +0200

    code restructure

--- src/components/routes/Session.vue
+++ src/components/routes/Session.vue
@@ -15,12 +15,14 @@
     <div class="status">
       <h3 class="mv3 h3">Status</h3>
         <ul class="lsn mv3">
-          <li>socket: {{ status }}</li>
-          <li>privateId: {{ session.privateId }}</li>
-          <li>publicId: {{ session.publicId }}</li>
+          <li>socket: {{ connection.status }}</li>
+          <li>privateId: {{ connection.sessionIds.privateId }}</li>
+          <li>publicId: {{ connection.sessionIds.publicId }}</li>
         </ul>
         <h3 class="mv3 h3">Log</h3>
-        <div>{{logData}}</div>
+        <ul class="lsn">
+          <li class="mv3" v-for="logEntry in logData" :key="logEntry">{{logEntry}}</li>
+        </ul>
     </div>
 
   </div>
@@ -33,25 +35,27 @@ export default {
   name: 'Session',
   data: () => {
     return {
+      connection: {
+        status: 'initial',
+        sessionIds: {},
+        socket: undefined
+      },
       connectionDetails: {
         host: '192.168.0.7',
         port: '8888',
         protocol: 'ws'
       },
-      status: '',
-      session: {},
-      socket: null,
-      logData: ''
+      logData: []
     }
   },
   methods: {
     ping() {
-      if (this.socket) {
-        this.socket.send('ping')
+      if (this.connection.socket) {
+        this.connection.socket.send('ping')
       }
     },
     log(msg) {
-      this.logData += `\n${msg}`
+      this.logData.push(msg)
     },
     newSession() {
       axios.get(`http://${this.connectionDetails.host}:${this.connectionDetails.port}/new`)
@@ -64,30 +68,31 @@ export default {
           return
         }
 
-        this.session = response.data
-        this.connectionDetails.sessionId = this.session.privateId
+        this.connection.sessionIds = response.data
+        this.connectionDetails.sessionId = this.connection.sessionIds.privateId
         this.connect()
       })
       .catch((error) => {
         this.log(error)
-        this.status = 'network error'
+        this.connection.status = 'network error'
       })
       .finally(function () {
         // always executed
       })
     },
     connect() {
-      this.socket = new WebSocket(`${this.connectionDetails.protocol}://${this.connectionDetails.host}:${this.connectionDetails.port}/?id=${this.connectionDetails.sessionId}`)
-      this.socket.onopen = () => {
+      this.connection.status = 'connecting'
+      this.connection.socket = new WebSocket(`${this.connectionDetails.protocol}://${this.connectionDetails.host}:${this.connectionDetails.port}/?id=${this.connectionDetails.sessionId}`)
+      this.connection.socket.onopen = () => {
         this.log("[open] Connection established")
-        this.status = 'connected'
+        this.connection.status = 'connected'
       }
-      this.socket.onmessage = (event) => {
+      this.connection.socket.onmessage = (event) => {
         this.log(`[message] Data received from server: ${event.data}`)
       }
-      this.socket.onclose = (event) => {
+      this.connection.socket.onclose = (event) => {
         this.log('Connection closed: ', event.code, event.reason)
-        this.status = 'closed'
+        this.connection.status = 'closed'
       }
     }
   }
